49d5228440fe3571ce74e27c865bb551b5e271ea
Sergey Muravskiy <0717376@gmail.com>
2023-05-05 18:54:14 +0300

    Initial commit

--- app.py
+++ app.py
@@ -15,5 +15,28 @@ def search():
     results = [item for item in data if query.lower() in item['name'].lower()]
     return jsonify(results)
 
+
+
+@app.route('/statistics', methods=['GET'])
+def statistics():
+    with open('tasks-hard-collection.europlan.ru-20230505T091738-040.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    unique_inn = set()
+    contract_count = 0
+    total_delay_sum = 0
+
+    for item in data:
+        unique_inn.add(item["inn"])
+        for contract in item["contractInfo"]:
+            contract_count += 1
+            total_delay_sum += contract["paymentDelaySumNdsInContractCurrency"]
+
+    return jsonify({
+        "unique_inn_count": len(unique_inn),
+        "contract_count": contract_count,
+        "total_delay_sum": total_delay_sum
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
